b3a65b4baafc007bba104ea626545350c329f161
Srilekha Palleboyina <srilekha091@gmail.com>
2018-12-18 07:45:01 -0800

    Vertical columns + Add New Member code

--- Taskboard/Scripts/Script.js
+++ Taskboard/Scripts/Script.js
@@ -35,24 +35,18 @@ var app = angular.module("TaskBoardApp", ["ngRoute", 'ui.bootstrap', 'ngDragDrop
 
     })
     .controller("dragDropController", function ($scope, $http, $routeParams, $route) {
-
-
-        $scope.startCallback = function (event, ui) {
-            console.log('You started draggin');
-        };
-
-        $scope.stopCallback = function (event, ui) {
-            console.log('Why did you stop draggin me?');
-        };
-
+        
         $scope.dragCallback = function (event, ui, taskId) {
-            console.log('hey, look I`m flying');
             console.log(taskId);
             $scope.TaskBeingDragged = parseInt(taskId.taskId);
         };
 
         $scope.dropCallback = function (event, ui, employeeId) {
-            console.log('hey, you dumped me :-(');
+            if (employeeId.employeeId == 0)
+            {
+                return;
+            }
+
             $scope.DraggedToEmployee = parseInt(employeeId.employeeId);
             
             $http({
@@ -67,12 +61,10 @@ var app = angular.module("TaskBoardApp", ["ngRoute", 'ui.bootstrap', 'ngDragDrop
         };
 
     })
-    /*writing controller for pop up modal form*/
+    /* Controller for pop up modal form for adding new project*/
     .controller("modalAccountFormController", function ($scope, $modal, $log, $route) {
 
             $scope.showForm = function () {
-                $scope.message = "Modal pop up window has been clicked";
-                console.log($scope.message);
 
                 var modalInstance = $modal.open({
                     templateUrl: 'Templates/CreateProject.html',
@@ -88,10 +80,38 @@ var app = angular.module("TaskBoardApp", ["ngRoute", 'ui.bootstrap', 'ngDragDrop
                 modalInstance.result.then(function (selectedItem) {
                     $scope.selected = selectedItem;
                 }, function () {
-                    $log.info('Modal dismissed at: ' + new Date());
+                    $log.info('Modal dismissed');
                     });
             };
-        });
+    })
+
+    /* end of add new project controller */
+
+    /* Controller for pop up modal form for adding new member*/
+    .controller("addNewMemberFormController", function ($scope, $modal, $log, $route) {
+
+        $scope.showForm = function () {
+
+            var addNewMemberInstance = $modal.open({
+                templateUrl: 'Templates/AddNewMember.html',
+                controller: AddNewMemberInstanceCtrl,
+                scope: $scope,
+                resolve: {
+                    userForm: function () {
+                        return $scope.userForm;
+                    }
+                }
+            });
+
+            addNewMemberInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed');
+            });
+        };
+    });
+
+    /* end of add new member controller */
 
         var ModalInstanceCtrl = function ($route, $scope, $modalInstance, userForm) {
             $scope.form = {}
@@ -132,7 +152,42 @@ var app = angular.module("TaskBoardApp", ["ngRoute", 'ui.bootstrap', 'ngDragDrop
             };
         };
 
-    /* end of controller */
+    var AddNewMemberInstanceCtrl = function ($route, $scope, $addNewMemberInstance, userForm) {
+            $scope.form = {}
+            $scope.submitForm = function () {
+                if ($scope.form.userForm.$valid) {
+
+                    var newEmployeeData = {
+                        EmployeeName: $scope.form.userForm.EmployeeName.$modelValue
+                    };
+
+
+                    var jsonData = JSON.stringify(newEmployeeData);
+
+                    $.ajax({
+                        url: "TaskManagerService.asmx/AddNewEmployee",
+                        method: "post",
+                        data: '{employee : ' + jsonData + '}',
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json"
+                    })
+                        .then(function (response) {
+                            console.log("Successfully added new employee");
+                            $route.reload();
+                        }, function (response) {
+                            console.log("Failed to add new employee");
+                        })
+
+                    $modalInstance.close('closed');
+                } else {
+                    console.log('userform is not in scope');
+                }
+            };
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        };
 
 
 function myFunction() {
